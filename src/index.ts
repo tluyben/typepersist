b978eff18137ef83340e42c16941adda92d816ba
tluyben <tycho@e-lab.nl>
2025-04-21 14:13:26 +0200

    removed type

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 export { CoreDB } from "./core-db";
 export { CoreDBPlus } from "./core-db-plus";
 export { Cmp, Wrapper as DB } from "./core-js-wrapper";
-export {
-  And,
-  createTypeSafeDB,
-  eq,
-  gt,
-  gte,
-  inOp,
-  like,
-  lt,
-  lte,
-  neq,
-  Or,
-  SortBy,
-  TypeSafeDB,
-  Where,
-} from "./type-safe-wrapper";
+// export {
+//   And,
+//   createTypeSafeDB,
+//   eq,
+//   gt,
+//   gte,
+//   inOp,
+//   like,
+//   lt,
+//   lte,
+//   neq,
+//   Or,
+//   SortBy,
+//   TypeSafeDB,
+//   Where,
+// } from "./type-safe-wrapper";
 export { User } from "./user-storage-types";
